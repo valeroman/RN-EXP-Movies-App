4d9a9f855ea68bf1c13c0951ac209c2687365e30
valeroman <valeroman@gmail.com>
2024-11-23 19:39:27 -0500

    05-pantalla-carga-safearea-manual

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,16 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator } from 'react-native'
 import React from 'react'
 import { useMovies } from '@/presentation/hooks/useMovies'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const HomeScreen = () => {
 
+    const safeArea = useSafeAreaInsets();
     const { nowPlayingQuery } = useMovies();
 
-
+    if ( nowPlayingQuery.isLoading ) {
+        return (
+            <View className='justify-center items-center flex-1'>
+                <ActivityIndicator color="purple" size={40} />
+            </View>
+        )
+    }
+    
     return (
-        <View>
-            <Text>HomeScreen</Text>
-            <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+        <View className='mt-2' style={{ paddingTop: safeArea.top }}>
+            <Text className='text-3xl font-bold px-4 mb-2'>HomeScreen</Text>
         </View>
     )
 }
